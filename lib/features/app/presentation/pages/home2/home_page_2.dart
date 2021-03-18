52b7abc2cd081aee1cbc36d23848241fa0648894
Gregory Iscala <greko11maister@gmail.com>
2021-03-18 18:29:14 -0500

    renderizado de card

--- lib/features/app/presentation/pages/home2/home_page_2.dart
+++ lib/features/app/presentation/pages/home2/home_page_2.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+import 'package:rick_and_morty2/features/app/presentation/widgets/character_card_widget.dart';
 class HomePage2 extends StatelessWidget {
   static const routeName = "/home_page2";
   @override
@@ -26,27 +27,29 @@ class HomePage2 extends StatelessWidget {
           })
         ],
       ),
-      body: Column(children: [
-        Padding(
-          padding: const EdgeInsets.all(8.0).copyWith(top: 30),
-          child: Text("The Rick and Morty API",
-            style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 50,), textAlign: TextAlign.center,),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        decoration: BoxDecoration(
+          color: Colors.black87
         ),
-        Expanded(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            decoration: BoxDecoration(
-              color: Colors.black87
-            ),
-            child: ListView.builder(
-                itemCount: 5,
-                itemBuilder: (context, i){
-                  return Placeholder(fallbackHeight: 200,);
-             // return CharacterCard();
-            }),
-          ),
-        )
-      ],),
+        child: ListView.builder(
+            itemCount: 5,
+            itemBuilder: (context, i){
+              if(i == 0){
+                return Column(children: [Container(
+                  color: Colors.white,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0).copyWith(top: 30),
+                    child: Text("The Rick and Morty API",
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 50,), textAlign: TextAlign.center,),
+                  ),
+                ),
+                  CharacterCard()
+                ],);
+              }
+         return CharacterCard();
+        }),
+      ),
     );
   }
 
--- lib/features/app/presentation/widgets/character_card_widget.dart
+++ lib/features/app/presentation/widgets/character_card_widget.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
+
 class CharacterCard extends StatelessWidget {
   final String url;
 
-   CharacterCard({Key key, this.url = "https://cdn.pixabay.com/photo/2017/06/28/04/29/adult-2449725_960_720.jpg"}) : super(key: key);
+  CharacterCard({Key key, this.url = "https://cdn.pixabay.com/photo/2017/06/28/04/29/adult-2449725_960_720.jpg"})
+      : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -16,42 +19,57 @@ class CharacterCard extends StatelessWidget {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-           ClipRRect(
-               borderRadius: BorderRadius.vertical(
-                 top: Radius.circular(20)
-               ),
-               child: Image.network(url)),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text("Baby Legs", style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),),
+           // ClipRRect(borderRadius: BorderRadius.vertical(top: Radius.circular(20)), child: Image.network(url)),
+            ClipRRect(
+                borderRadius: BorderRadius.vertical(
+                  top: Radius.circular(20)
+                ),
+                child: Image.network(url)),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text(
+                "Baby Legs",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Colors.white),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
+                  Icon(
+                    MdiIcons.moonFull,
+                    size: 12,
+                    color: Colors.green,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 6),
+                    child: Text(
+                      "Alive - Human",
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(top: 20, bottom: 4),
+              child: Text("Last know location:", style: TextStyle(color: Colors.grey, fontSize: 14)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0),
+              child: Text("Intermensional Cable", style: TextStyle(color: Colors.white, fontSize: 16)),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0).copyWith(top: 20, bottom: 4),
+              child: Text("First see in:", style: TextStyle(color: Colors.grey, fontSize: 14)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 8.0, bottom: 10),
+              child: Text("Rixty Minutes", style: TextStyle(color: Colors.white, fontSize: 16)),
+            ),
+          ],
         ),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Row(children: [
-            Icon(MdiIcons.moonFull, size: 12, color: Colors.green,),
-             Padding(
-               padding: const EdgeInsets.only(left: 6),
-               child: Text("Alive - Human", style: TextStyle(color: Colors.white),),
-             )
-          ],),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0).copyWith(top: 20, bottom: 4),
-          child: Text("Last know location:", style: TextStyle(color: Colors.grey, fontSize: 14)),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left:8.0),
-          child: Text("Intermensional Cable", style: TextStyle(color: Colors.white, fontSize: 16)),
-        ),
-        Padding(
-          padding: const EdgeInsets.all(8.0).copyWith(top: 20, bottom: 4),
-          child: Text("First see in:", style: TextStyle(color: Colors.grey, fontSize: 14)),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 8.0, bottom: 10),
-          child: Text("Rixty Minutes", style: TextStyle(color: Colors.white, fontSize: 16)),
-        ),
-      ],),),
+      ),
     );
   }
 }
